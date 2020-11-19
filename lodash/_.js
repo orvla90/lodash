923391a3d3567d323b3e4785ba56c31c321309d8
Alvaro <alvarobl@libera-t.com>
2020-11-19 13:17:47 +0100

    completed

--- lodash/_.js
+++ lodash/_.js
@@ -34,10 +34,90 @@ const _ = {
         list_words.push(string)
         return list_words;
     },
+    pad(string, num){
+        if (num > string.length){
+            return " ".repeat(Math.floor((num-string.length)/2)) + string + ' '.repeat(Math.round((num-string.length)/2));
+        };
+        return string;
+    },
+    has(object, parameter){
+        let result = false;
+        const keys = Object.keys(object);
+        for (let index = 0; index < keys.length; index++) {
+            if (keys[index] === parameter) {
+                result = true;
+            };
+        };
+        return result;
+    },
+    invert(object){
+        let inverted = {};
+        const keys = Object.keys(object);
+        for (let index = keys.length-1; index >= 0; index--) {
+            inverted[object[keys[index]]] = keys[index];
+        };
+        return inverted;
+    },
+    findKey(object, predicate){
+        let keys = Object.keys(object);
+        for (let index = 0; index < keys.length; index++) {
+            let key = keys[index];
+            let value = object[keys[index]];
+            if (predicate(value)){
+                return key;
+            };
+        };
+        return undefined;
+    },
+    drop(array, num){
+        if (num === undefined) {
+            num = 1;
+        };
+        let popped = [];
+        for (let index = num; index < array.length; index++) {
+            popped.push(array[index]);
+        };
+        return popped;
+    },
+    dropWhile(array, predicate){
+        let finalIndex = 0;
+        for (let index = 0; index < array.length; index++) {
+            if (!predicate(array[index], index)) {
+                finalIndex = index;
+                break
+            };
+        };
+        return _.drop(array, finalIndex);
+    },
+    dropWhile(array, predicate){
+        let finalIndex = 0;
+        for (let index = 0; index < array.length; index++) {
+            if (!predicate(array[index], index)) {
+                finalIndex = index;
+                break
+            };
+        };
+        return _.drop(array, finalIndex);
+    },
+    chunk(array, num){
+      if (num === undefined) {
+            num = 1;
+        };
+        let finalArray = [];
+        for (let index = 0; index < array.length; index+=num) {
+            let tempArray = [];
+            let count = 0;
+            while (count<num && array.length > count+index) {
+                tempArray.push(array[index+count]);
+                count++;
+            };
+            finalArray.push(tempArray);
+        };
+        return finalArray;
+    },
 };
 
 
-console.log(_.words('Hi hello matias'))
 
 
 // Do not write or modify code below this line.
